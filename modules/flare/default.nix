1f5941c2b544a5b0f66df8684feb37ec80522da6
IceDBorn <git.outsider841@simplelogin.fr>
2025-02-07 18:30:10 +0200

    flare: silence notifications

--- modules/flare/default.nix
+++ modules/flare/default.nix
@@ -6,8 +6,12 @@
 {
   home.packages = [ pkgs.flare-signal ];
 
-  dconf.settings."de/schmidhuberj/Flare" = {
-    notifications = true;
-    run-in-background = true;
+  dconf.settings = {
+    "de/schmidhuberj/Flare" = {
+      notifications = true;
+      run-in-background = true;
+    };
+
+    "org/sigxcpu/feedbackd/application/de-schmidhuberj-flare".profile = "silent";
   };
 }
