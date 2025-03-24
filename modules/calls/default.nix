79dcb0be89facb164d4b6af5e628dc45ce6cafdd
IceDBorn <git.outsider841@simplelogin.fr>
2025-03-24 22:01:15 +0000

    calls: use systemd

--- modules/calls/default.nix
+++ modules/calls/default.nix
@@ -4,18 +4,70 @@
   ...
 }:
 
-let
-  inherit (lib) filterAttrs;
-
-  getModules =
-    path:
-    builtins.map (dir: ./. + ("/modules/" + dir)) (
-      builtins.attrNames (filterAttrs (n: v: v == "directory") (builtins.readDir path))
-    );
-in
 {
-  imports = getModules (./modules);
-  home.packages = [ pkgs.calls ];
+  home.packages = with pkgs; [
+    alsa-utils
+    calls
+    inotify-tools
+  ];
 
-  nixpkgs.overlays = [ (import ./fix-call-audio-patch.nix) ];
+  nixpkgs.overlays = [
+    (final: super: {
+      calls = super.calls.overrideAttrs (
+        finalAttrs: superAttrs: {
+          patches = [
+            ./patch.nix
+          ];
+
+          doCheck = false;
+          doInstallCheck = false;
+        }
+      );
+    })
+  ];
+
+  systemd.user.services = {
+    gnome-calls-startup = {
+      Unit.Description = "Gnome calls";
+      Install.WantedBy = [ "graphical-session.target" ];
+
+      Service = {
+        ExecStart = "${pkgs.calls}/bin/gnome-calls --daemon";
+        Nice = "-20";
+        Restart = "on-failure";
+        StartLimitIntervalSec = 60;
+        StartLimitBurst = 60;
+      };
+    };
+
+    call-audio-handler-startup = {
+      Unit.Description = "Call audio handler";
+      Install.WantedBy = [ "graphical-session.target" ];
+
+      Service = {
+        ExecStart = "${
+          let
+            inherit (lib) makeBinPath;
+
+            pythonFile = builtins.toFile "script.py" ''
+              ${builtins.readFile ./call-audio-handler.py}
+            '';
+          in
+          with pkgs;
+          makeBinPath [
+            (writeShellScriptBin "call-audio-handler" ''
+              export PATH=$PATH:${lib.makeBinPath [ python3 ]}
+
+              python3 ${pythonFile}
+            '')
+          ]
+        }/call-audio-handler";
+
+        Nice = "-20";
+        Restart = "on-failure";
+        StartLimitIntervalSec = 60;
+        StartLimitBurst = 60;
+      };
+    };
+  };
 }
