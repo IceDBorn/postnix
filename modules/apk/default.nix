84d7360470d760fd9910b4dbb8076227040b3b08
IceDBorn <git.outsider841@simplelogin.fr>
2025-03-28 01:11:58 +0200

    chatty: use nix package and add systemd service to fix dbus launch

--- modules/apk/default.nix
+++ modules/apk/default.nix
@@ -6,7 +6,7 @@
 {
   home.packages = [
     (pkgs.writeShellScriptBin "remove-packages" ''
-      sudo apk del gnome-software flatpak papers firefox-esr gnome-calculator gnome-calendar calls snapshot celluloid gnome-clocks gnome-contacts loupe gnome-maps gnome-text-editor gnome-weather postmarketos-welcome decibels showtime postmarketos-tweaks
+      sudo apk del gnome-software flatpak papers firefox-esr gnome-calculator gnome-calendar calls snapshot celluloid gnome-clocks gnome-contacts loupe gnome-maps gnome-text-editor gnome-weather postmarketos-welcome decibels showtime postmarketos-tweaks chatty
     '')
 
     (pkgs.writeShellScriptBin "install-packages" ''
--- modules/chatty/default.nix
+++ modules/chatty/default.nix
@@ -1,3 +1,29 @@
 {
+  pkgs,
+  ...
+}:
+
+let
+  package = pkgs.chatty;
+in
+{
+  home.packages = [ package ];
   dconf.settings."org/sigxcpu/feedbackd/application/sm-puri-chatty".profile = "silent";
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "olm-3.2.16"
+  ];
+
+  systemd.user.services.chatty = {
+    Unit.Description = "Chatty";
+
+    Service = {
+      BusName = "sm.puri.Chatty";
+      ExecStart = "${package}/bin/chatty --gapplication-service";
+      Nice = "-20";
+      Restart = "on-failure";
+      StartLimitBurst = 60;
+      Type = "dbus";
+    };
+  };
 }
