837b5dfcc4b0f8caa74934af9aa8aeed60bb8cdf
IceDBorn <git.outsider841@simplelogin.fr>
2025-03-28 02:03:30 +0200

    flare: fix service exiting

--- modules/flare/default.nix
+++ modules/flare/default.nix
@@ -3,11 +3,31 @@
   ...
 }:
 
+let
+  package = pkgs.flare-signal;
+in
 {
-  home.packages = [ pkgs.flare-signal ];
+  home.packages = [ package ];
+
+  systemd.user.services.flare = {
+    Unit.Description = "Flare";
+
+    Service = {
+      BusName = "de.schmidhuberj.Flare";
+      ExecStart = "${package}/bin/flare --gapplication-service";
+      Nice = "-20";
+      Restart = "on-failure";
+      StartLimitBurst = 60;
+      Type = "dbus";
+    };
+  };
 
   dconf.settings = {
-    "de/schmidhuberj/Flare".notifications = true;
+    "de/schmidhuberj/Flare" = {
+      notifications = true;
+      run-in-background = true;
+    };
+
     "org/sigxcpu/feedbackd/application/de-schmidhuberj-flare".profile = "silent";
   };
 }
